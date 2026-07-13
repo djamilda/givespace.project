004d652a1da108da7a4537716cce78a498be5f57
hasbyheker-dev <241230040@unmuhpnk.ac.id>
2026-07-13 14:13:46 +0700

    new brach login

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/admin/login', function () {
+        return view('auth.admin-login');
+    })->name('admin.login');
+});
+
+require __DIR__.'/auth.php';
